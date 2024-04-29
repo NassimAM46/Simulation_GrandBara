d9d780e1eb9840c7f377b41e976c14bd98792c0f
NassimAM46 <nassimali537@gmail.com>
2024-04-29 14:41:34 +0200

    Version 1

---
+++ GBplot.py
@@ -0,0 +1,13 @@
+import numpy as np
+from scipy.spatial import Voronoi, voronoi_plot_2d
+from matplotlib import pyplot as plt
+#import plotly.express as px
+
+points=np.random.rand(200,2)
+gb=Voronoi(points)
+
+plt.scatter(points[:,0],points[:,1])
+fig=voronoi_plot_2d(gb,show_points=0,show_vertices=0,line_width=2)
+plt.savefig("Simulation.png")
+plt.show()
+
