9a56356eb8ccdbe76a448ef3507a47e9faba002f
NassimAM46 <nassimali537@gmail.com>
2024-04-29 15:02:19 +0200

    version 2

--- GBplot.py
+++ GBplot.py
@@ -1,10 +1,23 @@
+
+
+# ======================================================================================================
+# Simulation : terre du grand bara
+# Code written by:
+#   M. Nassim AM
+#   Université de Bordeaux, France
+# See Copyright Notice in COPYRIGHT
+# HowTo in README.md
+# ======================================================================================================
+
 import numpy as np
 from scipy.spatial import Voronoi, voronoi_plot_2d
 from matplotlib import pyplot as plt
 #import plotly.express as px
 
+
 points=np.random.rand(200,2)
 gb=Voronoi(points)
+
 
 plt.scatter(points[:,0],points[:,1])
 fig=voronoi_plot_2d(gb,show_points=0,show_vertices=0,line_width=2)
